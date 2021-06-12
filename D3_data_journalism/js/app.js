875a73fe4474e9a8e5a91ce84f1e49dc811b14a9
Ian Arch <ian.arch01@gmail.com>
2021-06-12 09:11:25 -0600

    elements

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -8,3 +8,12 @@ var chartMargin = {
 };
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
