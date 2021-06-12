fd2ca6d677a64875cef0c13200998647fbf8e451
Ian Arch <ian.arch01@gmail.com>
2021-06-12 09:09:45 -0600

    chart dimensions

---
+++ D3_data_journalism/js/app.js
@@ -0,0 +1,10 @@
+var svgWidth = 960;
+var svgHeight = 660;
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
